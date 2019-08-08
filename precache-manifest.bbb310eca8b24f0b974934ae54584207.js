2465d3467a0139460ab162feb2106edf55315688
Daaimah Tibrey <dtibrey@surveymonkey.com>
2019-08-07 20:20:49 -0700

    Updates

---
+++ precache-manifest.bbb310eca8b24f0b974934ae54584207.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94acc12b9342f55a47fa",
+    "url": "/Daaimah_Portfolio/static/css/main.08ed50ef.chunk.css"
+  },
+  {
+    "revision": "94acc12b9342f55a47fa",
+    "url": "/Daaimah_Portfolio/static/js/main.94acc12b.chunk.js"
+  },
+  {
+    "revision": "baa4938e26578452de72",
+    "url": "/Daaimah_Portfolio/static/js/runtime~main.baa4938e.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/Daaimah_Portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "d9ca6c92bc95145db2d0f66bdc2c4df5",
+    "url": "/Daaimah_Portfolio/index.html"
+  }
+];
