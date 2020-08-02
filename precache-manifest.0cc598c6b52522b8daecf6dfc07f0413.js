21d3da075e65f25ef69bcbc49a8e02df35a4a64c
Daaimah Tibrey <dtibrey@surveymonkey.com>
2020-08-01 17:32:44 -0700

    Updates

--- precache-manifest.0cc598c6b52522b8daecf6dfc07f0413.js
+++ precache-manifest.0cc598c6b52522b8daecf6dfc07f0413.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "4ff61fdfadb23c7d3ab4",
-    "url": "/Daaimah_Portfolio/static/css/main.08ed50ef.chunk.css"
+    "revision": "c957b759a32387b4c1d2",
+    "url": "/Daaimah_Portfolio/static/css/main.80c3f479.chunk.css"
   },
   {
-    "revision": "4ff61fdfadb23c7d3ab4",
-    "url": "/Daaimah_Portfolio/static/js/main.4ff61fdf.chunk.js"
+    "revision": "c957b759a32387b4c1d2",
+    "url": "/Daaimah_Portfolio/static/js/main.c957b759.chunk.js"
   },
   {
     "revision": "baa4938e26578452de72",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/Daaimah_Portfolio/static/js/2.5ec21d73.chunk.js"
   },
   {
-    "revision": "6c7231c2103a7b7e2acef10463afc6e3",
+    "revision": "1ced0312956d46031d8cd16bc78a091e",
     "url": "/Daaimah_Portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Daaimah_Portfolio/precache-manifest.9ca4104c36a9c10b11d54a25bdd39182.js"
+  "/Daaimah_Portfolio/precache-manifest.0cc598c6b52522b8daecf6dfc07f0413.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/Daaimah_Portfolio/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
