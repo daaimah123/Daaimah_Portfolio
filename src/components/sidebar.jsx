18bb3d78820d01bd488d3992dd52949acffdf94e
Daaimah Tibrey <41805952+daaimah123@users.noreply.github.com>
2025-03-18 18:45:43 -0700

    Update sidebar.jsx IG handle

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -28,7 +28,7 @@ export default class Sidebar extends Component {
               <ul>
                 <li><a href="https://www.facebook.com/daaimahb" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
                 <li><a href="https://twitter.com/DTibrey" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
-                <li><a href="https://www.instagram.com/mama_dauma/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
+                <li><a href="https://www.instagram.com/dibrey/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
                 <li><a href="https://www.linkedin.com/in/daaimah123/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
                 <li><a href="https://github.com/daaimah123" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
                 <li><a href="https://medium.com/@DTibrey" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2"></i></a></li>
